adbde9243ba8eac40fe5b551d33b5ff2f9f06741
Mathias Gjerland <mathiasgjerland@gmail.com>
2025-02-28 14:30:21 +0100

    La til bilder og endret script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('nav-links').classList.toggle('active');
 });
 
-
+/* Todo: Place the footer at the bottom of the page using CSS */
 
